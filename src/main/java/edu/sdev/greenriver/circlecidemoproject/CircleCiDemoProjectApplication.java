7be2dbeb370bf20dfc39db5e5e267bb08d5dbfee
LothCat <Fernalsr@plu.edu>
2022-11-29 13:41:17 -0800

    created project

---
+++ src/main/java/edu/sdev/greenriver/circlecidemoproject/CircleCiDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package edu.sdev.greenriver.circlecidemoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleCiDemoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CircleCiDemoProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/sdev/greenriver/circlecidemoproject/CircleCiDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sdev.greenriver.circlecidemoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircleCiDemoProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
